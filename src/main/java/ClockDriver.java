3d7a8281d0e0f3725d9326cae575e614b716304c
mussyman24 <121067118+mussyman24@users.noreply.github.com>
2024-09-19 12:01:34 -0700

    Update ClockDriver.java
    done with clock driver

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,3 +1,5 @@
+package src.main.java;
+
 
 public class ClockDriver
 {
@@ -14,8 +16,41 @@ public class ClockDriver
         //  * Tick test for 01:59 to 02:00
         //  * Tick test for 09:59 to 10:00
         //  * Tick test for 23:59 to 00:00
+        ClockDisplay test1 = new ClockDisplay();
+        String test1Output = test1.getTime();
+        System.out.println("Empty Con. Test - " + test1Output);
+        
+        ClockDisplay test2 = new ClockDisplay(3,45);
+        String test2Output = test2.getTime();
+        System.out.println(" 2 arg. Con. test " + test2Output);
         
         
+        ClockDisplay tickTest1 = new ClockDisplay(3,32);
+        System.out.println("Your time before tick is: " + tickTest1.getTime());
+        tickTest1.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest1.getTime());
+        
+        ClockDisplay tickTest2 = new ClockDisplay(3,9);
+        System.out.println("Your time before tick is: " + tickTest2.getTime());
+        tickTest2.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest2.getTime());
+        
+        ClockDisplay tickTest3 = new ClockDisplay(1,59);
+        System.out.println("Your time before tick is: " + tickTest3.getTime());
+        tickTest3.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest3.getTime());
+        
+        ClockDisplay tickTest4 = new ClockDisplay(9,59);
+        System.out.println("Your time before tick is: " + tickTest4.getTime());
+        tickTest4.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest4.getTime());
+        
+        ClockDisplay tickTest5 = new ClockDisplay(23,59);
+        System.out.println("Your time before tick is: " + tickTest5.getTime());
+        tickTest5.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest5.getTime());
+        
+                
         //Implement tests for your ClockDisplaySeconds
         //  * Test that you can do the basics
         //      * Empty constructor & read time
@@ -27,6 +62,29 @@ public class ClockDriver
         //  * Tick test for 01:00:59 to 01:01:00
         //  * Tick test for 01:59:59 to 02:00:00
         //  * Tick test for 23:59:59 to 00:00:00
+        ClockDisplaySeconds tickTest1Sec = new ClockDisplaySeconds(3,32,59);
+        System.out.println("Your time before tick is: " + tickTest1Sec.getTime());
+        tickTest1Sec.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest1Sec.getTime());
+        
+        ClockDisplaySeconds tickTest2Sec = new ClockDisplaySeconds(1,0,59);
+        System.out.println("Your time before tick is: " + tickTest2Sec.getTime());
+        tickTest2Sec.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest2Sec.getTime());
+        
+        ClockDisplaySeconds tickTest3Sec = new ClockDisplaySeconds(1,59,59);
+        System.out.println("Your time before tick is: " + tickTest3Sec.getTime());
+        tickTest3Sec.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest3Sec.getTime());
+        
+        ClockDisplaySeconds tickTest4Sec = new ClockDisplaySeconds(23,59,59);
+        System.out.println("Your time before tick is: " + tickTest4Sec.getTime());
+        tickTest4Sec.timeTick();
+        System.out.println(" Your time after tick is: " + tickTest4Sec.getTime());
+        
+        
+        
+        
         
         /*CHALLENGE*/
         //Implement tests for your ClockDisplay12Hour
